38695bfaeded6f5e075bf44daf1d84f9c73904e6
Ryan <rsully.dev+git@me.com>
2011-08-12 11:15:52 -0400

    Initial Commit

---
+++ RSColorPicker/RSColorPickerAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  RSColorPickerAppDelegate.h
+//  RSColorPicker
+//
+//  Created by Ryan Sullivan on 8/12/11.
+//  Copyright 2011 Freelance Web Developer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RSColorPickerAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
---
+++ RSColorPicker/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  RSColorPicker
+//
+//  Created by Ryan Sullivan on 8/12/11.
+//  Copyright 2011 Freelance Web Developer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[])
+{
+    NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, nil);
+    [pool release];
+    return retVal;
+}
