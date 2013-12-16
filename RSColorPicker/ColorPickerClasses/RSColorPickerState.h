df12e1a4cd55d81fabe70152dd861a1f02258f4b
Alex Nichol <alexnichol@comcast.net>
2013-12-16 16:32:23 -0500

    added state comments

--- RSColorPicker/ColorPickerClasses/RSColorPickerState.h
+++ RSColorPicker/ColorPickerClasses/RSColorPickerState.h
@@ -27,18 +27,37 @@
 
 @property (readonly) CGFloat hue, saturation, brightness, alpha;
 
+/**
+ * Creates a state with a 1.0 alpha and 1.0 brightness that would arise
+ * by selecting `point` on a color picker of diameter `size` and padding `padding`.
+ */
 + (RSColorPickerState *)stateForPoint:(CGPoint)point size:(CGFloat)size padding:(CGFloat)padding;
 
+/**
+ * Create a state with a given color.
+ */
 - (id)initWithColor:(UIColor *)selectionColor;
+
+/**
+ * Create a state given a point on the unit circle and brightness+alpha
+ */
 - (id)initWithScaledRelativePoint:(CGPoint)p brightness:(CGFloat)V alpha:(CGFloat)A;
+
+/**
+ * Create a state given HSVA components.
+ */
 - (id)initWithHue:(CGFloat)H saturation:(CGFloat)S brightness:(CGFloat)V alpha:(CGFloat)A;
 
 - (UIColor *)color;
 
-// Calculates the position on a circular HSV color wheel.
+/**
+ * Returns the position of this state on a color picker of size `size` and padding `padding`.
+ * Note: this point may be outside of the unit circle if a point outside the unit circle
+ * was picked to generate this state.
+ */
 - (CGPoint)selectionLocationWithSize:(CGFloat)size padding:(CGFloat)padding;
 
-// modifications
+// This class is immutable, so these are helpful!
 - (RSColorPickerState *)stateBySettingBrightness:(CGFloat)newBright;
 - (RSColorPickerState *)stateBySettingAlpha:(CGFloat)newAlpha;
 - (RSColorPickerState *)stateBySettingHue:(CGFloat)newHue;
