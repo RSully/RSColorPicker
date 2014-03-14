c1d835056ad2239d4ab9443abc00e61a122066d6
Ryan <rsul.dev@me.com>
2014-03-13 21:31:30 -0400

    Yo dawg.
    We are "testing" our tests. Not sure if there is a right way to do this.

--- RSColorPickerTests/CPTestCase.m
+++ RSColorPickerTests/CPTestCase.m
@@ -13,6 +13,22 @@
 
 @implementation CPTestCase
 
+- (void)testAssertColorEqualsColor
+{
+    UIColor *redA = [UIColor redColor];
+    UIColor *redB = [UIColor colorWithRed:1.0 green:0.0 blue:0.0 alpha:1.0];
+
+    [self assertColor:redA equalsColor:redB];
+}
+
+- (void)testAssertColorNotEqualsColor
+{
+    UIColor *red = [UIColor redColor];
+    UIColor *green = [UIColor greenColor];
+
+    [self assertColor:red notEqualsColor:green];
+}
+
 #pragma mark - Component helpers
 
 - (void)assertColor:(UIColor *)colorA equalsColor:(UIColor *)colorB
