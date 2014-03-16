7109b9037983c4e68be79e43472c3e31b41b1b57
Ryan <rsul.dev@me.com>
2014-03-16 15:33:20 -0400

    Add color function tests

---
+++ RSColorPickerTests/RSColorFunctionsTests.m
@@ -0,0 +1,60 @@
+//
+//  RSColorFunctionsTests.m
+//  RSColorPicker
+//
+//  Created by Ryan Sullivan on 3/16/14.
+//
+//
+
+#import <XCTest/XCTest.h>
+#import "RSColorFunctions.h"
+
+@interface RSColorFunctionsTests : CPTestCase
+
+@end
+
+@implementation RSColorFunctionsTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+
+- (void)testPixelFromHSV {
+
+}
+
+- (void)testHSVFromPixel {
+
+}
+
+- (void)testComponentsForColor {
+
+}
+
+- (void)testImageWithScale {
+
+}
+
+- (void)testOpacityBackgroundImage {
+
+}
+
+- (void)testRandomColor {
+    CGFloat components[4];
+
+    UIColor *color = RSRandomColorOpaque(YES);
+
+    RSGetComponentsForColor(components, color);
+    XCTAssertEqual(components[3], 1.0);
+}
+
+@end
